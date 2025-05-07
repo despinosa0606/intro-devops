8b5dc47f4070c979f1be984d1cf0f7b933bbbe25
Daniel Espinosa <despinosa0606@gmail.com>
2025-05-07 07:59:25 -0500

    change credentials aws

--- codigos/0states.tf
+++ codigos/0states.tf
@@ -9,8 +9,8 @@ terraform{
 
 provider "aws" {
     region = "us-east-1"
-    #access_key = "AKIAYFQTFKR6I4IUOGSY"
-    #secret_key = "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"
+    access_key = "AKIARJBSM7FISRM4QTWP"
+    secret_key = "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"
 }
 #export AWS_ACCESS_KEY_ID=AKIAYFQTFKR6I4IUOGSY ; 
 #export AWS_SECRET_ACCESS_KEY=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
